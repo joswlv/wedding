60af54a5a64ccd2296252004567e01c67c832a6e
조승완/데이터테크랩/NP <joswlv@nhnent.com>
2019-07-15 09:19:32 +0900

    한남주차장 추가

--- js/main.js
+++ js/main.js
@@ -283,25 +283,44 @@
 			level: 3 // 지도의 확대 레벨
 		};
 
-		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 		var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-		var imageSrc = HOME_PATH + '/images/heart_marker_coral.png', // 마커이미지의 주소입니다    
-			imageSize = new kakao.maps.Size(50, 69), // 마커이미지의 크기입니다
-			imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-			
-		// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-		var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-			markerPosition = new kakao.maps.LatLng(37.5261600757218, 127.042301232111); // 마커가 표시될 위치입니다
+		var imageSrc = HOME_PATH + '/images/heart_marker_coral.png',
+			imageSize = new kakao.maps.Size(50, 69),
+			imageOption = {offset: new kakao.maps.Point(27, 69)};
+		
+		var parkingImgSrc = HOME_PATH + '/images/parking_icon.png',
+			parkingImgSize = new kakao.maps.Size(30,38),
+			parkingImgOption = {offset: new kakao.maps.Point(27, 38)};
 
-		// 마커를 생성합니다
-		var marker = new kakao.maps.Marker({
-			position: markerPosition, 
-			image: markerImage // 마커이미지 설정 
+		var parkingMarkerImg = new kakao.maps.MarkerImage(parkingImgSrc, parkingImgSize, parkingImgOption),
+			parkingMarkerPosition = new kakao.maps.LatLng(37.5249298061868, 127.045793906426);
+			
+		var hallMarkerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+			hallMarkerPosition = new kakao.maps.LatLng(37.5261600757218, 127.042301232111);
+
+		var parkingMarker = new kakao.maps.Marker({
+			position: parkingMarkerPosition,
+			image: parkingMarkerImg
 		});
 
-		// 마커가 지도 위에 표시되도록 설정합니다
-		marker.setMap(map);
+		var hallMarker = new kakao.maps.Marker({
+			position: hallMarkerPosition, 
+			image: hallMarkerImage 
+		});
+
+		var iwContent = '<div style="padding:5px;">셔틀버스 운행</div>',
+			iwPosition = new kakao.maps.LatLng(37.5249298061868, 127.045793906426); 
+
+		var infowindow = new kakao.maps.InfoWindow({
+			position : iwPosition, 
+			content : iwContent 
+		});
+		
+		parkingMarker.setMap(map);
+		infowindow.open(map, parkingMarker);
+		
+		hallMarker.setMap(map);
 	};
 
 	var initCountDown = function() {
